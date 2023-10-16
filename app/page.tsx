ca1a43295f9630f0cee25cc6d6cd59ffa515a900
dreamjser <dreamjser@gmail.com>
2023-10-16 17:26:12 +0800

    feat: layout模板

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Next.js',
+}
 
 export default function Page() {
   return (
     <>
       <h1><Link href="/login">登录</Link></h1>
-
     </>
   )
 }
