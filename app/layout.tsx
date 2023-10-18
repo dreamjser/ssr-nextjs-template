f3110e30d4a36ceb519df9d0aae817cd7879b7f1
dreamjser <dreamjser@gmail.com>
2023-10-18 16:20:05 +0800

    feat: 集成antd

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,12 @@ import React, { ReactNode } from 'react'
 
 export default function RootLayout({
     children,
-    header,
-    nav,
   }: {
     children: ReactNode,
-    header: ReactNode,
-    nav: ReactNode
   }) {
     return (
       <html lang="en">
         <body>
-          {header}
-          {nav}
           {children}
         </body>
       </html>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+'use client'
 
+import React from 'react'
+import Button from '../../components/button'
 export default function Page() {
-  return <h1>Hello, login</h1>
+  return <h1>Hello, login <Button onClick={() => {alert('111')}}>按钮</Button></h1>
 }
