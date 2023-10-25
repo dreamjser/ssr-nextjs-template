7bae9f953b4a57f7141b6be197e99ab6f1e1f37f
dreamjser <dreamjser@gmail.com>
2023-10-25 17:38:48 +0800

    feat: fetch请求数据

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
 import Link from 'next/link'
 import { Metadata } from 'next'
+import { NavBar } from '../ui'
 
-export const metadata: Metadata = {
-  title: 'Next.js',
+const metadata: Metadata = {
+  title: 'Next学习',
 }
 
-export default function Page() {
+async function getData() {
+  const res = await fetch(`${process.env.BASE_URL}getHomeBanner`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Page() {
+  const res = await getData()
+  const data = res.data || {}
+
   return (
     <>
+      <NavBar back={null}>首页</NavBar>
+      <div className="home-banner">
+        <img src={data.url} alt="home banner" />
+      </div>
       <h1 className="fs24"><Link href="/login">登录</Link></h1>
     </>
   )
---
+++ ui/index.ts
@@ -0,0 +1,3 @@
+'use client'
+
+export {NavBar} from 'antd-mobile'
