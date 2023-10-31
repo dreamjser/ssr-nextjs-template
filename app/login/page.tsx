89bd7771d28b0c09c30456e6450ae72dbef33a0b
dreamjser <dreamjser@gmail.com>
2023-10-31 09:13:05 +0800

    feat: useSearchParams

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,12 @@
 'use client'
 
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
 import {Button} from '../../ui'
+
 export default function Page() {
-  return <h1>Hello, login <Button onClick={() => {alert('111')}}>按钮</Button></h1>
+  const searchParams = useSearchParams()
+  const params = searchParams.get('a')
+
+  return <h1>Hello, login {JSON.stringify(params)} <Button onClick={() => {alert('111')}}>按钮</Button></h1>
 }
